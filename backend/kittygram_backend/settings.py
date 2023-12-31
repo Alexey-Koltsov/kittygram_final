df189a0e4802e4bbdf65b689ebf68773426fb07d
Алексей Кольцов <koltsov2022@yandex.ru>
2023-12-31 04:12:00 +0300

    DEBUG = False

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,9 +9,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True if 'true' == os.getenv('DEBUG').lower() else False
+DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('SECRET_KEY').split()
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split()
 
 INSTALLED_APPS = [
     'django.contrib.admin',
